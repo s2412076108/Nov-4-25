ceaecd63bf6773af737cdf63c3a2f1d0113eaad2
Nirnita Banik <s2412076108@ru.ac.bd>
2025-11-04 23:28:00 +0600

    d.cpp commit

---
+++ d.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    vector<int>d={5,2,5,7,2,9,7,10};
+    unordered_set<int>s;
+    for(int x:d) if(!s.count(x)) cout<<x<<" ", s.insert(x);
+}
+
